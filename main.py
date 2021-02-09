d7b31d2f6f23bd672929d62612d7c9e5acdd664a
ReidBarbeln2022 <814684@mystma.org>
2021-02-09 16:05:30 +0000

    find dubles try 1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def findDoubles(x):
+   
+
+    for i in range (0, len(x) - 1):
+        if x[i] == x[i + 1]: 
+            return True
+    return False
+print (findDoubles("madagascar"))
